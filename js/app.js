10a806320bfd5019898c03327d8f76220329dc3d
Albert Nazander <alizbazar@alizweb.com>
2015-02-07 22:43:12 +0200

    Write the app logic

--- js/app.js
+++ js/app.js
@@ -44,6 +44,18 @@ define({
     }
 });
 
+function getNextTask(callback) {
+    // GET NEXT ITEM
+    // CACHE it's data
+    console.log('start');
+    var a = Math.random();
+    if (a > 0.5) {
+        switchView('incoming-start');
+    } else {
+        switchView('outgoing-start');
+    }
+}
+
 // Relevant divs:
 // incoming-start incoming-select incoming-transfer incoming-more outgoing-start outgoing-map outgoing-transfer
 // .task-content, .task-actions
@@ -53,21 +65,42 @@ function switchView(view) {
 }
 
 $('.ui-page').on('click', '.ui-btn', function(e) {
-    var context = $(this).closest('footer').data('context');
+    var $btn = $(this);
+    var context = $btn.closest('footer').data('context');
     console.log('context:' + context);
 
     if (context == 'incoming-start') {
         switchView('incoming-select');
+        // Switch .task-content text with item name when in proximity
+        // toggle disabled state of the button
+        setTimeout(function() { $('.task-actions.incoming-select .ui-btn').removeAttr('disabled') }, 1000);
+
     } else if (context == 'incoming-select') {
+        // TODO: select item, send to server
         switchView('incoming-transfer');
+
+
     } else if (context == 'incoming-transfer') {
+        // TODO: get crane location, save location to item data 
         switchView('incoming-more');
     } else if (context == 'incoming-more') {
-        switchView('outgoing-start');
+        // TODO: identify yes / no
+        if ($btn.hasClass('confirm-btn')) {
+            console.log('yes');
+            switchView('incoming-start');
+        } else {
+            // TODO: no => mark task complete && getNextItem
+            getNextTask();
+        }
     } else if (context == 'outgoing-start') {
+        // TODO: set task status to STARTED && get own location & crane location, draw crane, item & me on the map && display item identifier
         switchView('outgoing-map');
     } else if (context == 'outgoing-map') {
+        // TODO: change item status as to LEAVING_STORAGE
         switchView('outgoing-transfer');
+    } else if (context == 'outgoing-transfer') {
+        // TODO: remove item from storage && getNextItem
+        getNextTask();
     }
 });
 
