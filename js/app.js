3856f2114266807ec9796dc117b844aff8d649bb
Albert Nazander <alizbazar@alizweb.com>
2015-02-07 21:49:45 +0200

    Add benning of UI-wiring

--- js/app.js
+++ js/app.js
@@ -44,3 +44,30 @@ define({
     }
 });
 
+// Relevant divs:
+// incoming-start incoming-select incoming-transfer incoming-more outgoing-start outgoing-map outgoing-transfer
+// .task-content, .task-actions
+function switchView(view) {
+    $('.task-content, .task-actions').not('.' + view).toggleClass('hidden', true);
+    $('.task-content.' + view + ', .task-actions.' + view).toggleClass('hidden', false);
+}
+
+$('.ui-page').on('click', '.ui-btn', function(e) {
+    var context = $(this).closest('footer').data('context');
+    console.log('context:' + context);
+
+    if (context == 'incoming-start') {
+        switchView('incoming-select');
+    } else if (context == 'incoming-select') {
+        switchView('incoming-transfer');
+    } else if (context == 'incoming-transfer') {
+        switchView('incoming-more');
+    } else if (context == 'incoming-more') {
+        switchView('outgoing-start');
+    } else if (context == 'outgoing-start') {
+        switchView('outgoing-map');
+    } else if (context == 'outgoing-map') {
+        switchView('outgoing-transfer');
+    }
+});
+
